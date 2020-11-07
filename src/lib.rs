a825d2227eaeadb3bf8393da4983348ddb2ae2f2
Ian McIntyre <ianpmcintyre@gmail.com>
2020-11-07 10:18:01 -0500

    Fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -58,14 +58,14 @@
 //! ```
 //!
 //! ### License
-//! 
+//!
 //! Licensed under either of
-//! 
+//!
 //! - [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0) ([LICENSE-APACHE](./LICENSE-APACHE))
 //! - [MIT License](http://opensource.org/licenses/MIT) ([LICENSE-MIT](./LICENSE-MIT))
-//! 
+//!
 //! at your option.
-//! 
+//!
 //! Unless you explicitly state otherwise, any contribution intentionally submitted
 //! for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
 //! dual licensed as above, without any additional terms or conditions.
