5f553b570c68b77dd14ed5d9e7a0022d37e4d3a6
Ian McIntyre <ianpmcintyre@gmail.com>
2020-11-07 10:08:12 -0500

    Add licenses

--- src/lib.rs
+++ src/lib.rs
@@ -56,6 +56,19 @@
 //!
 //! assert_eq!(destination, [5;32]);
 //! ```
+//!
+//! ### License
+//! 
+//! Licensed under either of
+//! 
+//! - [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0) ([LICENSE-APACHE](./LICENSE-APACHE))
+//! - [MIT License](http://opensource.org/licenses/MIT) ([LICENSE-MIT](./LICENSE-MIT))
+//! 
+//! at your option.
+//! 
+//! Unless you explicitly state otherwise, any contribution intentionally submitted
+//! for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+//! dual licensed as above, without any additional terms or conditions.
 
 #![no_std]
 
