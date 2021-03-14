593434bee521b69d51e91487dc2de12e4afed604
Ian McIntyre <ianpmcintyre@gmail.com>
2021-03-14 07:58:17 -0400

    Add bidirectional peripheral with full-duplex support

--- src/peripheral.rs
+++ src/peripheral.rs
@@ -44,16 +44,12 @@ pub unsafe trait Source<E: Element> {
     /// Perform any actions necessary to enable DMA transfers
     ///
     /// Callers use this method to put the peripheral in a state where
-    /// it can supply the DMA channel with data. This method may change
-    /// the peripheral's state, and the implementation will ensure that
-    /// any mutations are atomic.
-    fn enable_source(&self);
+    /// it can supply the DMA channel with data.
+    fn enable_source(&mut self);
     /// Perform any actions necessary to disable or cancel DMA transfers
     ///
-    /// This may include undoing the actions in `enable_source`. This
-    /// method may change the peripheral's state, and the implementation
-    /// will ensure that any mutations are atomic.
-    fn disable_source(&self);
+    /// This may include undoing the actions in `enable_source`.
+    fn disable_source(&mut self);
 }
 
 /// A peripheral that can be the destination for DMA data
@@ -88,16 +84,12 @@ pub unsafe trait Destination<E: Element> {
     /// Perform any actions necessary to enable DMA transfers
     ///
     /// Callers use this method to put the peripheral into a state where
-    /// it can accept transfers from a DMA channel. This method may change
-    /// the peripheral's state, and the implementation will ensure that
-    /// any mutations are atomic.
-    fn enable_destination(&self);
+    /// it can accept transfers from a DMA channel.
+    fn enable_destination(&mut self);
     /// Perform any actions necessary to disable or cancel DMA transfers
     ///
-    /// This may include undoing the actions in `enable_destination`.  This
-    /// method may change the peripheral's state, and the implementation
-    /// will ensure that any mutations are atomic.
-    fn disable_destination(&self);
+    /// This may include undoing the actions in `enable_destination`.
+    fn disable_destination(&mut self);
 }
 
 /// A DMA transfer that receives data from hardware
@@ -109,7 +101,7 @@ where
     E: Element,
 {
     channel: &'a Channel,
-    source: &'a S,
+    source: &'a mut S,
     transfer: Transfer<'a>,
     _elem: PhantomData<E>,
 }
@@ -141,7 +133,7 @@ where
 /// Use a DMA channel to receive a `buffer` of elements from the source peripheral
 pub fn receive<'a, S, E>(
     channel: &'a mut Channel,
-    source: &'a S,
+    source: &'a mut S,
     buffer: &'a mut [E],
 ) -> Rx<'a, S, E>
 where
@@ -173,7 +165,7 @@ where
     E: Element,
 {
     channel: &'a Channel,
-    destination: &'a D,
+    destination: &'a mut D,
     transfer: Transfer<'a>,
     _elem: PhantomData<E>,
 }
@@ -206,7 +198,7 @@ where
 pub fn transfer<'a, D, E>(
     channel: &'a mut Channel,
     buffer: &'a [E],
-    destination: &'a D,
+    destination: &'a mut D,
 ) -> Tx<'a, D, E>
 where
     D: Destination<E>,
@@ -228,5 +220,131 @@ where
         // Safety: transfer is correctly defined
         transfer: unsafe { Transfer::new(channel) },
         _elem: PhantomData,
+    }
+}
+
+/// Indicates that a peripheral can read and write from a single buffer
+/// using two simultaneous DMA transfers
+///
+/// It's expected that the TX operation will drive the RX operation.
+/// A SPI transfer can be modeled in this manner.
+///
+/// # Safety
+///
+/// `Bidirectional` assumes the same safety requirements as source and
+/// destination. Addtionally, you ensure that the peripheral is capable
+/// of this kind of transfer from a single buffer.
+pub unsafe trait Bidirectional<E: Element>: Source<E> + Destination<E> {}
+
+/// A full-duplex DMA transfer from a single buffer
+///
+/// `FullDuplex` only works with [`Bidirectional`](crate::peripheral::Bidirectional)
+/// peripherals. The transfer acts on a single buffer, sending and receiving data
+/// element by element.
+pub struct FullDuplex<'a, P, E>
+where
+    P: Bidirectional<E>,
+    E: Element,
+{
+    rx_channel: &'a Channel,
+    rx_transfer: Transfer<'a>,
+    rx_done: bool,
+    tx_channel: &'a Channel,
+    tx_transfer: Transfer<'a>,
+    tx_done: bool,
+    peripheral: &'a mut P,
+    _elem: PhantomData<E>,
+}
+
+/// Perform a full-suplex DMA transfer using two DMA channels
+/// that read and write from a single buffer.
+pub fn full_duplex<'a, P, E>(
+    rx_channel: &'a mut Channel,
+    tx_channel: &'a mut Channel,
+    peripheral: &'a mut P,
+    buffer: &'a mut [E],
+) -> FullDuplex<'a, P, E>
+where
+    P: Bidirectional<E>,
+    E: Element,
+{
+    // RX channel takes data from the peripheral, and moves it to the buffer..
+    rx_channel.set_channel_configuration(ChannelConfiguration::enable(peripheral.source_signal()));
+    super::set_source_hardware(rx_channel, peripheral.source_address());
+    super::set_destination_linear_buffer(rx_channel, buffer);
+    rx_channel.set_minor_loop_bytes(core::mem::size_of::<E>() as u32);
+    rx_channel.set_transfer_iterations(buffer.len() as u16);
+
+    // TX channel takes data from the buffer, and sends it to the peripheral
+    tx_channel.set_channel_configuration(ChannelConfiguration::enable(
+        peripheral.destination_signal(),
+    ));
+    super::set_source_linear_buffer(tx_channel, buffer);
+    super::set_destination_hardware(tx_channel, peripheral.destination_address());
+    tx_channel.set_minor_loop_bytes(core::mem::size_of::<E>() as u32);
+    tx_channel.set_transfer_iterations(buffer.len() as u16);
+
+    FullDuplex {
+        rx_channel,
+        rx_transfer: unsafe { Transfer::new(rx_channel) },
+        rx_done: false,
+        tx_channel,
+        tx_transfer: unsafe { Transfer::new(tx_channel) },
+        tx_done: false,
+        peripheral,
+        _elem: PhantomData,
+    }
+}
+
+impl<P, E> Future for FullDuplex<'_, P, E>
+where
+    P: Bidirectional<E>,
+    E: Element,
+{
+    type Output = Result<(), ErrorStatus>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        // Schedule the receive first...
+        if !self.rx_done {
+            // Safety: pin projection OK, no movement from future...
+            let poll = unsafe {
+                self.as_mut()
+                    .map_unchecked_mut(|this| &mut this.rx_transfer)
+            }
+            .poll(cx)?;
+            // Safety: OK to toggle a bool...
+            *unsafe { &mut self.as_mut().get_unchecked_mut().rx_done } = poll.is_ready();
+        }
+
+        if !self.tx_done {
+            // Safety: pin projection OK, no movement from future...
+            let poll = unsafe {
+                self.as_mut()
+                    .map_unchecked_mut(|this| &mut this.tx_transfer)
+            }
+            .poll(cx)?;
+            // Safety: OK to toggle a bool...
+            *unsafe { &mut self.as_mut().get_unchecked_mut().tx_done } = poll.is_ready();
+        }
+
+        if self.tx_done && self.rx_done {
+            Poll::Ready(Ok(()))
+        } else {
+            Poll::Pending
+        }
+    }
+}
+
+impl<P, E> Drop for FullDuplex<'_, P, E>
+where
+    P: Bidirectional<E>,
+    E: Element,
+{
+    fn drop(&mut self) {
+        self.peripheral.disable_destination();
+        self.peripheral.disable_source();
+        while self.tx_channel.is_hardware_signaling() {}
+        while self.rx_channel.is_hardware_signaling() {}
+        // Drop the transfers to finish cancellation...
     }
 }
