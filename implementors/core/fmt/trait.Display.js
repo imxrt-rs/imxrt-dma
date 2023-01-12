6b57780d7fabe9c74b6c0b0ee7c59c56f25b6137
mciantyre <mciantyre@users.noreply.github.com>
2023-01-12 19:40:24 +0000

    deploy: dd4c11ad075d552558a2b6c34edb9f17b3fc7a3d

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"imxrt_dma":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"imxrt_dma/struct.Error.html\" title=\"struct imxrt_dma::Error\">Error</a>"]]
+"imxrt_dma":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"imxrt_dma/struct.Error.html\" title=\"struct imxrt_dma::Error\">Error</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
